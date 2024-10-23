8f6f93e1d1957710b2e9c2370be7701cab4fa20b
Rayan Silva Lima <rayan.lima@shopdelivery.com.br>
2024-10-23 14:49:40 -0300

    dark mode default

--- assets/js/theme-change.js
+++ assets/js/theme-change.js
@@ -1,7 +1,13 @@
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 const currentTheme = localStorage.getItem('theme');
 
-if (currentTheme) {
+// Se não houver tema armazenado, define o modo escuro como padrão
+if (!currentTheme) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+    toggleSwitch.checked = true; // Marca o checkbox para corresponder ao tema
+} else {
+    // Se houver tema armazenado, aplica o tema correto
     document.documentElement.setAttribute('data-theme', currentTheme);
   
     if (currentTheme === 'dark') {
@@ -13,10 +19,10 @@ function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
     }
-    else {        document.documentElement.setAttribute('data-theme', 'light');
-          localStorage.setItem('theme', 'light');
-    }    
 }
 
 toggleSwitch.addEventListener('change', switchTheme, false);
