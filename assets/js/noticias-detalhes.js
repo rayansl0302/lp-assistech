eeb95e76358b624c2a4a835247593ec26f14dcfd
Rayan Silva <rayansl0302@gmail.com>
2024-10-31 15:54:10 -0300

    noticias dinamicas

--- assets/js/noticias-detalhes.js
+++ assets/js/noticias-detalhes.js
@@ -1,33 +1,35 @@
 document.addEventListener("DOMContentLoaded", async function () {
     const urlParams = new URLSearchParams(window.location.search);
-    const postId = parseInt(urlParams.get("id"));
+    const postTitle = urlParams.get("title");
 
     try {
-        const response = await fetch("https://api.npoint.io/53058604c773f1e8accc");
+        const response = await fetch("https://api-staging.assistechpublicacoes.com.br/v1/public/news");
         if (!response.ok) throw new Error("Erro ao buscar dados da API");
-        
+
         const postData = await response.json();
-        const post = postData.find(item => item.id === postId);
+
+        // Procura o post com o título correspondente ao parâmetro "title"
+        const post = postData.data.find(item => item.title.toLowerCase().replace(/[\s\W-]+/g, '-').replace(/^-+|-+$/g, '') === postTitle);
 
         if (post) {
             document.querySelector(".post-title").textContent = post.title;
             document.querySelector(".post-date").textContent = post.date;
-            document.querySelector(".post-hour").textContent = post.hour;
+            document.querySelector(".post-author").textContent = post.author;
 
             const mainImage = document.querySelector(".main-image");
-            if (mainImage && post.images[0]) {
-                mainImage.src = post.images[0];
-                mainImage.alt = "Imagem principal";
+            if (mainImage && post.image_url) {
+                mainImage.src = post.image_url;
+                mainImage.alt = post.image_label || "Imagem principal";
             }
 
             const paragraphsContainer = document.querySelector(".post-paragraphs");
             paragraphsContainer.innerHTML = "";
-            post.paragraphs.forEach(paragraph => {
-                const pElement = document.createElement("p");
-                pElement.classList.add("mb-4");
-                pElement.textContent = paragraph;
-                paragraphsContainer.appendChild(pElement);
-            });
+
+            // Adiciona o conteúdo como um único parágrafo
+            const pElement = document.createElement("p");
+            pElement.classList.add("mb-4");
+            pElement.textContent = post.content;
+            paragraphsContainer.appendChild(pElement);
         } else {
             console.error("Post não encontrado");
         }
