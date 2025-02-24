a4486b5f7c64fcce80a96578ac1e84b59d2f5d45
Rayan Silva <rayansl0302@gmail.com>
2025-02-24 19:04:43 -0300

    vercel correção

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,16 +1,22 @@
-// Configuration object for API endpoints
-const API_CONFIG = {
+// config.js
+var API_CONFIG = {
   BASE_URL: 'https://api.assistechpublicacoes.com.br',
   STAGING_URL: 'https://api-staging.assistechpublicacoes.com.br',
   APP_URL: 'https://app.assistechpublicacoes.com.br',
   API_VERSION: 'v1'
 };
 
-// Function to get the API URL based on staging flag
-export const getApiUrl = (staging = false) => {
+function getApiUrl(staging = false) {
   const baseUrl = staging ? API_CONFIG.STAGING_URL : API_CONFIG.BASE_URL;
   return `${baseUrl}/${API_CONFIG.API_VERSION}/public`;
-};
+}
 
-// Function to get the app URL
-export const getAppUrl = () => API_CONFIG.APP_URL;
+function getAppUrl() {
+  return API_CONFIG.APP_URL;
+}
+
+// Attach to global window object
+window.CONFIG = {
+  getApiUrl,
+  getAppUrl
+};
